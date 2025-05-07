e7ba88531c65a6f210fe82e72fb87795f3cb3698
MindMatrix-Shril <shril@clinf.com>
2025-05-07 13:12:09 +0530

    Minor code changes and yaml files for gcp app engine hosting

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Handle SPA routing - send all requests to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
