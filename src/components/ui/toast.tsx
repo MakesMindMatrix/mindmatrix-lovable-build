93bba3f2707f215857af0a45922506f2c7776296
MindMatrix-Shril <shril@clinf.com>
2025-05-10 16:35:15 +0530

    Added hsl to correct toasts

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -27,9 +27,9 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border border-gray-200 bg-background text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive border-destructive bg-destructive text-destructive-foreground",
       },
     },
     defaultVariants: {
