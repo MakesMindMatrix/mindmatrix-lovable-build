78192f7a7a97c428f91c321db2f1ad1d00c0539a
MindMatrix-Shril <shril@clinf.com>
2025-05-10 15:21:49 +0530

    Clicking on mindmatrix logo redirects to "/"

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,14 +1,17 @@
 
 import React from "react";
 import LogoIcon from "./LogoIcon";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="flex gap-2 items-center self-center text-2xl font-semibold tracking-tighter whitespace-nowrap text-slate-800">
+      <Link to="/" className="flex gap-2 items-center">
       <div className="object-contain shrink-0 self-stretch my-auto aspect-[1.65] w-[38px]">
-        <LogoIcon />
+          <LogoIcon />
       </div>
       <h1 className="self-stretch my-auto">MindMatrix</h1>
+      </Link>
     </header>
   );
 };
